d2f111c7baaff5dd3d9ddf5a2779e16df9d551da
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2023-03-29 16:29:45 +0200

    Alu

---
+++ CPU_ADA.srcs/sources_1/new/function_unit.vhd
@@ -0,0 +1,72 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.NUMERIC_STD.ALL;
+
+
+entity function_unit is
+port(   a_bus      : in std_logic_vector(31 downto 0);
+        b_bus      : in std_logic_vector(31 downto 0);
+        s          : in std_logic_vector(1 downto 0);
+        t          : in std_logic_vector(1 downto 0);
+        c_in       : in std_logic;
+        m          : in std_logic_vector(1 downto 0);
+        c, z, n, p : out std_logic;
+        d_out      : out std_logic_vector(31 downto 0)  
+        );
+end function_unit;
+
+architecture Behavioral of function_unit is
+subtype vector is std_logic_vector(31 downto 0);
+subtype vector_2 is std_logic_vector(32 downto 0);
+
+signal adder_out, condition_out, logic_out, shift_out, mux_out : vector;
+signal operand1, operand2 : signed(31 downto 0);
+
+--signal nn : unsigned(4 downto 0);
+
+signal result, carry : signed(32 downto 0);
+constant zeroes : signed(31 downto 0) := (others => '0');
+
+begin
+    -- definizioni
+    operand1 <= signed(a_bus);
+    operand2 <= signed(condition_out);
+
+    carry <= (zeroes & c_in);
+     
+    -- b conditioning
+    condition_out <= (others => '0') when s = "00" else
+                     (b_bus) when s = "01" else
+                     (not b_bus) when s = "10" else
+                     (others => '1') when s = "11" else
+                     (others => 'X');
+     -- parallel adder               
+     result <= (a_bus(31) & operand1) + (condition_out(31) & operand2) + carry;
+     c <= result(32);
+     adder_out <= vector(result(31 downto 0));
+     
+     -- logic unit
+     logic_out <= a_bus and b_bus when s = "00" else
+                  a_bus or b_bus when s = "01" else
+                  a_bus xor b_bus when s = "10" else
+                  not a_bus when s = "11" else
+                  (others => 'X');
+                  
+      -- barell shifter
+      shift_out <= vector(shift_left( unsigned(b_bus) , to_integer(unsigned(s & c_in & t)))) or vector(shift_right( unsigned(b_bus) , (32-to_integer(unsigned(s & c_in & t))))); 
+                    
+      -- multiplexers
+      mux_out <= shift_out when m(1) = '1' else
+               adder_out when m(0) = '0' else
+               logic_out when m(0) = '1' else
+               (others => 'X');
+      d_out <= mux_out;
+      
+        -- flags
+      z <= '1' when mux_out = vector(zeroes) else
+           '0';
+      n <= mux_out(31);
+        
+      
+          
+end Behavioral;
--- CPU_ADA.srcs/sources_1/new/register_file.vhd
+++ CPU_ADA.srcs/sources_1/new/register_file.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 28.03.2023 12:17:19
--- Design Name: 
--- Module Name: register_file - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity register_file is
     Port ( dcd_in   : in INTEGER range 0 to 31;
@@ -41,7 +12,7 @@ entity register_file is
            b_select : in INTEGER range 0 to 31;
            c_select : in STD_LOGIC;
            constant_in  : in STD_LOGIC_VECTOR (31 downto 0);
-           reg_in   : in STD_LOGIC_VECTOR (31 downto 0);
+           d_in   : in STD_LOGIC_VECTOR (31 downto 0);
            a_bus    : out STD_LOGIC_VECTOR (31 downto 0);
            b_bus    : out STD_LOGIC_VECTOR (31 downto 0)
            );
@@ -62,7 +33,7 @@ begin
         if res = '0' then
             registers <= (others => (others =>'0'));
         elsif rising_edge(clk) then
-            registers(write) <= reg_in;
+            registers(write) <= d_in;
         end if;
     end process;
 end Behavioral;
